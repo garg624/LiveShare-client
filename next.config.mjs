97a134a7f1acd5b2c9a34ffda6a9beb69eb024cb
garg624 <138591794+garg624@users.noreply.github.com>
2024-04-23 02:25:33 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    env:{
-        CLIENT_DOMAIN_NAME:"http://localhost:3000"
-    }
+    
 };
 // next.config.js
 // next.config.js
